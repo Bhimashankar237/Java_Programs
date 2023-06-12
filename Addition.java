9979948d5619925693ceec8cbf4eb76f24eaff18
Bhimashankar237 <136358832+Bhimashankar237@users.noreply.github.com>
2023-06-12 21:17:36 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+//wap to print addition of two numbers
+
+class Addition
+{
+	public static void main(String args[])
+	{
+		int a=5,b=6;
+		int c = a+b;
+		System.out.println(c);
+		
+	}
+}
---
+++ AssignmentOperators.java
@@ -0,0 +1,9 @@
+//WRITE A JAVA PROGRAM TO DEMO ASSIGNMENT OPERATOR?
+
+class AssignmentOperators
+{
+	public static void main(String args[])
+	{
+		System.out.println(" ");
+	}
+}
